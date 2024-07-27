65acf7b9b3157a8129dd08bd5a6aa44425ad1102
Umar Izzuddin <izzuddinumar13@gmail.com>
2024-07-27 19:58:59 +0700

    feat: create redeem reward API

--- src/api/reward/reward.module.ts
+++ src/api/reward/reward.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import RewardService from "./reward.service";
 import RewardController from "./reward.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import RewardEntity from "src/model/reward.entity";
+import { UserEntity } from "src/model/user.entity";
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([
+        RewardEntity,
+        UserEntity
+    ])],
     controllers: [RewardController],
     providers: [RewardService],
 })
--- src/api/reward/reward.service.ts
+++ src/api/reward/reward.service.ts
@@ -1,10 +1,53 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import RewardEntity, { RewardTier } from "src/model/reward.entity";
+import { UserEntity } from "src/model/user.entity";
+import { Repository } from "typeorm";
+
+const REWARD_PRICE = {
+    [RewardTier.ONE]: 100,
+    [RewardTier.TWO]: 200,
+    [RewardTier.THREE]: 300
+}
 
 @Injectable()
 export default class RewardService {
-    constructor() {}
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepo: Repository<UserEntity>,
+        @InjectRepository(RewardEntity)
+        private readonly rewardRepo: Repository<RewardEntity>
+    ) { }
 
     async redeemReward(userId: string) {
-        
+        const user = await this.userRepo.findOne({
+            where: {
+                id: userId
+            }
+        })
+
+        if (!user) {
+            throw new BadRequestException('User not found');
+        }
+
+        const rewards = await this.rewardRepo.find({
+            where: {
+                tier: user.tier
+            }
+        })
+
+        if(user.balance < REWARD_PRICE[user.tier]) {
+            throw new BadRequestException('Insufficient balance');
+        }
+
+        const randomIndex = Math.floor(Math.random() * rewards.length);
+
+        const reward = rewards[randomIndex];
+
+        user.balance = user.balance - REWARD_PRICE[user.tier];
+
+        await this.userRepo.save(user);
+
+        return reward;
     }
 }
